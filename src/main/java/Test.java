ece42a41a007ac8cb74ff7b0fefbd8d861afd6cf
luorenxin <luorenxin@jd.com>
2015-09-09 16:33:31 +0800

    pom提交

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: luorenxin
+ * Date: 15-9-9
+ * Time: обнГ4:33
+ * To change this template use File | Settings | File Templates.
+ */
+public class Test {
+}
